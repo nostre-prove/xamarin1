6e6a32b105a9169b61ab177629fd36c4550565c7
domenico.caputo <domenico.caputo@dbalab.it>
2018-04-04 10:24:47 +0200

    feat: change login layout; rename pages

---
+++ Test/Test/Pages/EngagementPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EngagementPage : ContentPage
+	{
+		public EngagementPage()
+		{
+			InitializeComponent ();
+		}
+
+        async void OnClicked_Logout(object sender, EventArgs e)
+        {
+            // AnalyticsHelper.Send("Logout", "Return to Login");
+            LoginService.RemoveUserInfo();
+            Navigation.InsertPageBefore(new LoginPage(), this);
+            await Navigation.PopAsync();
+        }
+
+    }
+}
--- Test/Test/Pages/MainPage.xaml.cs
+++ Test/Test/Pages/MainPage.xaml.cs
@@ -14,18 +14,23 @@ namespace Test.Pages
 			InitializeComponent ();            
             IsPresented = false;
             MasterBehavior = MasterBehavior.Popover;
-            Detail = new NavigationPage(new SiteInspectionPage());
+            
+            Detail = new NavigationPage(new EngagementPage())
+            {
+                BarBackgroundColor = Color.FromHex("#1F9BE3"),
+                BarTextColor = Color.FromHex("#FFF")
+            };
         }
 
         protected override void OnAppearing()
         {
-            var username = Application.Current.Properties.ContainsKey(Constants.USER_NAME) ? Application.Current.Properties[Constants.USER_NAME] : "ospite";
-            LabelMenuText.Text = String.Format("Benvenuto, {0}", username);
+            // var username = Application.Current.Properties.ContainsKey(Constants.USER_NAME) ? Application.Current.Properties[Constants.USER_NAME] : "ospite";
+            // LabelMenuText.Text = String.Format("Benvenuto, {0}", username);
         }
 
-        private void OnClicked_SiteInspection(object sender, EventArgs e)
+        private void OnClicked_Engagement(object sender, EventArgs e)
         {
-            Detail = new NavigationPage(new SiteInspectionPage());
+            Detail = new NavigationPage(new EngagementPage());
             IsPresented = false;
         }
 
@@ -34,25 +39,13 @@ namespace Test.Pages
             Detail = new NavigationPage(new SurveyPage());
             IsPresented = false;
         }
-
-        private void OnClicked_Calendar(object sender, EventArgs e)
-        {
-            Detail = new NavigationPage(new CalendarPage());
-            IsPresented = false;
-        }
-
+        
         private void OnClicked_Post(object sender, EventArgs e)
         {
             Detail = new NavigationPage(new PostPage());
             IsPresented = false;
         }
-
-        private void OnClicked_About(object sender, EventArgs e)
-        {
-            Detail = new NavigationPage(new AboutPage());
-            IsPresented = false;
-        }
-
+        /*
         async void OnClicked_Logout(object sender, EventArgs e)
         {
             AnalyticsHelper.Send("Logout", "Return to Login");
@@ -60,5 +53,6 @@ namespace Test.Pages
             Navigation.InsertPageBefore(new LoginPage(), this);
             await Navigation.PopAsync();
         }
+        */
     }
 }
